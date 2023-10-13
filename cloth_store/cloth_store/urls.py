a36e1a4d668d28ca5e62f10cd2a80520c5b06f7b
Avens Tach <artur.tymchuK@gmail.com>
2023-10-13 15:55:20 +0300

    delete and rename project

--- cloth_store/cloth_store/urls.py
+++ cloth_store/cloth_store/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cloth_shopo project.
+URL configuration for cloth_store project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/

65ed232799e446c898c62c087a9a8bfc48156eed
Avens_Tach <artur.tymchuk@gmail.com>
2023-10-17 21:39:54 +0300

    base.html

--- cloth_store/cloth_store/settings.py
+++ cloth_store/cloth_store/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'cloth_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cloth_store',
+        'USER': 'avens_tach',
+        'PASSWORD': 'paswd123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

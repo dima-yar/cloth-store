4f11086e8bb27e7c0ef3159c833de8815f5684db
Avens_Tach <artur.tymchuk@gmail.com>
2023-12-01 10:54:05 +0200

    load pisun lenth

--- generator/generator.py
+++ generator/generator.py
@@ -1,14 +1,13 @@
-import os, sys
-sys.setrecursionlimit(100000)
-folder_path = r"D:/images"
+import os
+
+folder_path = r"C:\Users\Користувач.KAB22-3\PycharmProjects\cloth-store\tempdata\images"
 list_of_files = os.listdir(folder_path)
 
 print(list_of_files)
-f = open(r"C:/Users/iryna/PycharmProjects/cloth-store/tempdata/Images.sql", 'w', encoding="utf-8")
+f = open(r"C:\Users\Користувач.KAB22-3\PycharmProjects\cloth-store\tempdata\Images.sql", 'w', encoding="utf-8")
 for i in list_of_files:
-    file_path = r"D:/images"+'/'+i
+    file_path = folder_path + '/' + i
     file_id = i[0:-4]
     f.write(f"INSERT INTO store_ProductImages (id, product_image) VALUES ({file_id}, lo_import('{str(file_path)}'));\n")
-
 
 f.close()
